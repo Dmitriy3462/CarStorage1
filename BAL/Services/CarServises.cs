3d18b5c733944c001ad64c1ee4b86caa61bb9ee3
eawydanil <pavliuchienko.danil@gmail.com>
2023-05-18 12:55:58 +0300

    FindCar, NewCar

--- BAL/Services/CarServises.cs
+++ BAL/Services/CarServises.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,8 +17,11 @@ namespace CarStorage.BAL.Services
 
         public void NewCar(Car car)
         {
-            if (car == null)
-               new ArgumentNullException("Поле не может быть пустым",nameof(car));
+            
+            if (car.CarModel == null || car.Year == null)
+                new ArgumentException("Отсутсвует название или год выпуска");
+            if (car.Year.Year < 1900 || car.Year.Year > DateTime.Now.Year)
+                new ArgumentException("Год выпуска введен неверно");                   
             var a = carRepository.GetAll();
             foreach (var item in a)
             {
@@ -26,13 +30,24 @@ namespace CarStorage.BAL.Services
                     new ArgumentException("Такая машина уже существует");
                 }
             }
+            carRepository.Create(car);
 
             carRepository.Save();
         }
-
-        public void Update(Car car)
+        public Car FindCar (int carId)
         {
-
+            var b = carRepository.GetAll();
+            foreach (var item in b)
+            {
+                if (item.Id == carId)
+                {
+                    return item;
+                }
+            }
+            Console.WriteLine("Машина не найдена");
+            return null;
         }
+
+        
     }
 }
