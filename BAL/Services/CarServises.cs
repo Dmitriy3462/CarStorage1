dfbdd97394ba9686f94ee70ba80c7c007f5c10fc
eawydanil <pavliuchienko.danil@gmail.com>
2023-05-18 16:33:39 +0300

    CarService + CategoryService.

--- BAL/Services/CarServises.cs
+++ BAL/Services/CarServises.cs
@@ -1,11 +1,13 @@
 ﻿using CarStorage.DAL.Models;
 using CarStorage.DAL.Models.Context;
 using CarStorage.DAL.Repositories;
+using CarStorage.BAL.Helpers;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,39 +17,104 @@ namespace CarStorage.BAL.Services
     {
         CarRepository carRepository = new CarRepository(new CarStorageContext());
 
+
+        #region DeleteCar
+        public void DeleteCar(int carId)
+        {
+            try
+            {
+                var car = carRepository.GetById(carId).FirstOrDefault();
+                if (car == null)
+                    throw new CarException("Car is not found");
+                 
+                carRepository.Delete(car);
+                carRepository.Save();
+                Console.WriteLine("Car has been successfully deleted.");
+            }
+            catch (CarException ex)
+            {
+                throw new CarException("Error when removing a car: " + ex.Message);
+            }
+        }
+        #endregion
+
+        #region UpdateCar
+        public void UpdateCar(Car car)
+        {
+            try
+            {
+                if (car == null)
+                {
+                    throw new CarException("Car is update");
+                }
+                var allCar = carRepository.GetAll();
+                foreach (var item in allCar)
+                {
+                    if (item.CarModel == car.CarModel && item.Year == car.Year)
+                    {
+                        throw new CarException("car with those name and year already exist");
+                    }
+                }
+                carRepository.Update(car);
+                carRepository.Save();
+
+            }
+            catch (CarException ex)
+            {
+                Console.WriteLine("Error update car: " + ex.Message);
+            }
+        }
+        #endregion
+
+        #region NewCar
         public void NewCar(Car car)
         {
-            
-            if (car.CarModel == null || car.Year == null)
-                new ArgumentException("Отсутсвует название или год выпуска");
-            if (car.Year.Year < 1900 || car.Year.Year > DateTime.Now.Year)
-                new ArgumentException("Год выпуска введен неверно");                   
-            var a = carRepository.GetAll();
-            foreach (var item in a)
+            try
             {
-                if (item.CarModel == car.CarModel&& item.Year == car.Year)
-                {
-                    new ArgumentException("Такая машина уже существует");
-                }
-            }
-            carRepository.Create(car);
+                if (car.CarModel == null || car.Year == null)
+                   throw new CarException ("Missing title or year of issue");                
 
-            carRepository.Save();
+                var existingCar = carRepository.GetAll().FirstOrDefault(item => item.CarModel == car.CarModel &&
+                item.Year == car.Year);
+
+                if (existingCar == null)
+                {
+                    carRepository.Create(car);
+                    carRepository.Save();
+                }
+                else
+                  throw new CarException("Such a machine already exists");                
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was a common mistake: " + ex.Message);
+            }
+
         }
-        public Car FindCar (int carId)
+        #endregion
+
+        #region FindCar
+        public Car FindCar(int carId)
         {
-            var b = carRepository.GetAll();
-            foreach (var item in b)
+            try
             {
-                if (item.Id == carId)
-                {
-                    return item;
-                }
+                var byId = carRepository.GetById(carId).FirstOrDefault();
+                if (byId == null)
+                    throw new CarException("Car not found");
+                
+                return byId;
             }
-            Console.WriteLine("Машина не найдена");
-            return null;
+            catch (CarException ex)
+            {
+                throw new CarException("Error occurred while finding the car: ");
+                return null;
+            }
         }
+        #endregion
 
-        
     }
+
+
 }
+
---
+++ BAL/Services/CategoryCervise.cs
@@ -0,0 +1,59 @@
+﻿using CarStorage.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarStorage.DAL.Models.Context;
+using CarStorage.BAL.Helpers;
+using CarStorage.DAL.Interfaces;
+using CarStorage.DAL.Models;
+
+namespace CarStorage.BAL.Services
+{
+    public class CategoryCervise
+    {
+        CategoryRepository categoryRepository = new CategoryRepository(new CarStorageContext());
+
+        #region DeleteCategory
+        public void DeleteCategory(int categoryId)
+        {
+            try
+            {
+                var category = carRepository.GetById(carId).FirstOrDefault();
+                if (category == null)
+                    throw new CarException("Car is not found");
+
+                carRepository.Delete(category);
+                carRepository.Save();
+                Console.WriteLine("Car has been successfully deleted.");
+            }
+            catch (CarException ex)
+            {
+                throw new CarException("Error when removing a category: " + ex.Message);
+            }
+        }
+        #endregion
+
+        #region UpdateCategory
+        public void UpdateCategory(Category category)
+        {
+            try
+            {
+                if (true)
+                {
+
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
+
+    }
+}
